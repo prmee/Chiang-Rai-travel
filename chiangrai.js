c198aab36f73a87634d5cb4ff0ddb22ce1608fcd
prmee <124676804+prmee@users.noreply.github.com>
2023-04-24 13:47:05 +0700

    Add files via upload

---
+++ chiangrai.js
@@ -0,0 +1,155 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดร่องขุ่น",
+          "type": "วัฒนธรรม",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            99.76339181349175,
+            19.824031348804652
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดร่องเสือเต้น",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            99.8418720000015,
+            19.92334339305502
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดห้วยปลากั้ง",
+          "type": "วัฒนธรรม",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            99.80694972883384,
+            19.949454425214526
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บ้านดำ",
+          "type": "พิพิธภัณฑ์",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            99.82472654232583,
+            19.914318349141908
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพระแก้ว",
+          "type": "วัฒนธรรม",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            99.82802849999888,
+            19.91180341255692
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ภูชี้ดาว",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.48125418650926,
+            19.870906090049175
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ไร่ชาฉุยฟง",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            99.81827600000065,
+            20.19916279302862
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เชียงรายไนท์บาร์ซ่า",
+          "type": "ตลาดนัด",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            99.83396699999889,
+            19.905019000000223
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Melt in your mouth",
+          "type": "ร้านอาหาร",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            99.83540999520255,
+            19.91741387715595
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "365 coffee",
+          "type": "ร้านอาหาร",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            99.86025784694499,
+            19.99255968486989
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
